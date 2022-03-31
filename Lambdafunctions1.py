2169a7467c779dd7868d6fb1553df919f544f795
Riya Tiwari <riyatiwari727@gmail.com>
2022-03-31 23:48:06 +0530

    commit2

---
+++ Lambdafunctions1.py
@@ -0,0 +1,27 @@
+# A Python program to filter a list of integers using Lambda.
+# original_nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# print("Original integers:", original_nums)
+# even_num = list(filter(lambda x: x%2 == 0, original_nums))
+# print("Even numbers:", even_num)
+# odd_num = list(filter(lambda x: x%2 != 0, original_nums))
+# print("Odd numbers:", odd_num)
+
+# A Python program to square and cube every number in a given list of integers using Lambda
+# original_nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# print("Original integers:", original_nums)
+# sq_num = list(map(lambda x: x**2, original_nums))
+# print(sq_num)
+# cube_num = list(map(lambda x: x**3, original_nums))
+# print(cube_num)
+
+# Write a Python program to find if a given string starts with a given character using Lambda
+
+# starts_with = lambda x: True if x.startswith('P') else False
+# print(starts_with('Preeti'))
+# starts_with = lambda x: True if x.startswith('P') else False
+# print(starts_with('Riya'))
+#
+# ends_with = lambda x: True if x.endswith('i') else False
+# print(ends_with('Preeti'))
+# ends_with = lambda x: True if x.endswith('j') else False
+# print(ends_with('Preeti'))
