b5845463bba0651a20170d7504a32dd31c8e0037
Riya Tiwari <riyatiwari727@gmail.com>
2022-04-03 21:06:25 +0530

    commit3

--- Usingmodules.py
+++ Usingmodules.py
@@ -4,15 +4,30 @@
 # print('playing sound using  playsound')
 
 # Find out the day of your birthdate in current year(Built-in module used)
-import datetime
-print("Find out the day of your birthdate in current year.")
-date_entry = input('Enter your current year birth date in DD-MM-YYYY format\n')
-year, month, day = map(int, date_entry.split('-'))
-date1 = datetime.date(day, month, year)
-print(date1.strftime("%A"))
+# import datetime
+# print("Find out the day of your birthdate in current year.")
+# date_entry = input('Enter your current year birth date in DD-MM-YYYY format\n')
+# day, month, year = map(int, date_entry.split('-'))
+# date1 = datetime.date(day, month, year)
+# print(date1.strftime("%A"))
 
+# print("Trying out random module.")
+# import random
+# a = random.randint(0, 10)
+# a = random.random()
+# a = random.randrange(10, 15)
+# print(a)
 
+# import random
+# lst = [0,1,2,3]
+#
+# for i in range(5):
+#     lst.append(random.random())
+#
+# print(lst)
 
-
-
-
+# import time
+# print(time.ctime(0))
+# print(time.ctime(100000))
+# print(time.time())
+# print(time.ctime(time.time()))
