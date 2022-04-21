c355d4a467487fce8f014d016e56842ae89210fd
Riya Tiwari <riyatiwari727@gmail.com>
2022-04-21 21:30:42 +0530

    6

--- StringpracticeExercises/Stringhexe6.py
+++ StringpracticeExercises/Stringhexe6.py
@@ -9,4 +9,17 @@
 # str1 = "r,i,y,a,t,i,w,a,r,i"
 # print(str1.rsplit(',',2))
 
+'''a Python program to find the first non-repeating character in given string.'''
+
+str1 = "abcdabc"
+def nonrepeated(str1):
+    freq = {}
+    for char in str1:
+        freq[char] = freq.get(char,0)+1
+    for i in str1:
+        if freq[i] == 1:
+            return i
+    return -1
+print(nonrepeated(str1))
+
 
