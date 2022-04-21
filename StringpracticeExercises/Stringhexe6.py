7ce850a5640ca83df6d3a186d039c18e3b79ab27
Riya Tiwari <riyatiwari727@gmail.com>
2022-04-21 09:50:25 +0530

    string5

---
+++ StringpracticeExercises/Stringhexe6.py
@@ -0,0 +1,12 @@
+# ''' to count and display the vowels of a given text'''
+# str = 'Favourite'
+# vowel = 'aeiouAEIOU'
+# def countvowel(str):
+#     return (len([char for char in str if char in vowel]))
+# print(countvowel(str))
+#
+# '''to split a string on the last occurrence of the delimiter.'''
+# str1 = "r,i,y,a,t,i,w,a,r,i"
+# print(str1.rsplit(',',2))
+
+
